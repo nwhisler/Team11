14f405bf96b048edc5cf059eb2b54faabae10094
Nick Whisler <nwhisl@gmail.com>
2024-03-23 12:14:11 -0600

    Added first table.

--- content/color.php
+++ content/color.php
@@ -1,40 +1,79 @@
 <!DOCTYPE html>
 <html>
     <?php
-        if((isset($_GET["dimensions"])) && isset($_GET["color"])) {
+        if((isset($_GET["dimensions"])) && isset($_GET["colors"])) {
 
             $dimensions = $_GET["dimensions"];
-            $color = $_GET["color"];
+            $colors = $_GET["colors"];
 
-            echo "<script> 
-                    let dimensions = $dimensions;
-                    let color = $color;
-                    if(dimensions < 1 || dimensions > 26) {
+            if(($dimensions < 1 || $dimensions > 26) && ($colors < 1 || $colors > 10)) {
+
+                echo "<script>
+                        window.alert('Incorrect dimensions and colors. Choose between 1 and 26 and 1 and 10.');
+                      </script>";                
+
+            }
+
+            elseif($dimensions < 1 || $dimensions > 26) {
+
+                echo "<script>
                         window.alert('Incorrect dimensions. Choose between 1 and 26.');
-                    }
-                    if(color < 1 || color > 10) {
+                      </script>";
+            
+            }
+
+            elseif($colors < 1 || $colors > 10) {
+
+                echo "<script>
                         window.alert('Incorrect colors. Choose between 1 and 10.');
-                    }
-                  </script>";
+                      </script>";
+                
+            }
+
+            else {
+
+                echo "<head>
+                        <meta charset='UTF-8'>
+                        <title>Color Coordinate Generation</title>
+                        <div class='content'>
+                            <nav>
+                                <li id='navbar'><a href='../index.php'>Home</a></li>
+                                <li id='navbar'><a href='about.php'>About</a></li>
+                                <li id='navbar'><a href='color.php'>Color Coordinate Generation</a></li>
+                            </nav>
+                    </head>
+                    <body>
+                        <div id='table-container'></div>
+                    </body>
+                    <footer>
+                        Copyright &#169 Team 11
+                    </footer>
+                    </div>
+                    <script> 
+                        let dimensions = $dimensions;
+                        let colors = $colors;
+                        if(dimensions < 1 || dimensions > 26) {
+                            window.alert('Incorrect dimensions. Choose between 1 and 26.');
+                        }
+                        if(colors < 1 || colors > 10) {
+                            window.alert('Incorrect colors. Choose between 1 and 10.');
+                        }
+                        let table = '<table>';
+                        for(let idx = 0; idx < $colors; idx++) {
+                            table += '<tr><td>Column</td><td>Column</td></tr>';    
+                        }
+                        table+= '</table>';
+                        document.getElementById('table-container').innerHTML = table;
+                    </script>";
+
+            }
 
         }
     ?>
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
-    <head>
-        <meta charset="UTF-8">
-    <title>Contact</title>
-    <div class="content">
-         <nav>
-            <li id='navbar'><a href='../index.php'>Home</a></li>
-            <li id='navbar'><a href='about.php'>About</a></li>
-            <li id='navbar'><a href='color.php'>Color Coordinate Generation</a></li>
-        </nav>
-        </head>
-    <body>
-        <!-- Tables -->
-    </body>
-    <footer>
-        Copyright &#169 Team 11
-    </footer>
-    </div>
+    <style type="text/css">
+        <?php
+            $css = file_get_contents("../css/style.css");
+            echo $css;
+        ?>
+    </style>
 </html> 
