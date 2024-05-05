7b726603670dac20d1295d1a586b5f4152c77f63
Stephen Klugherz <klugherz@colostate.edu>
2024-05-05 14:16:28 -0600

    fin

--- content/colors.php
+++ content/colors.php
@@ -2,48 +2,33 @@
 $colorsArray = array (
   0 => 
   array (
-    'Name' => 'Red',
-    'hex_value' => '#ff0000',
+    'Name' => '',
+    'hex_value' => 'asa',
   ),
   1 => 
-  array (
-    'Name' => 'Yellow',
-    'hex_value' => '#ffff00',
-  ),
-  2 => 
-  array (
-    'Name' => 'Green',
-    'hex_value' => '#00ff00',
-  ),
-  3 => 
-  array (
-    'Name' => 'Blue',
-    'hex_value' => '#0000ff',
-  ),
-  4 => 
   array (
     'Name' => 'Purple',
     'hex_value' => '#800080',
   ),
-  5 => 
+  2 => 
   array (
     'Name' => 'Grey',
     'hex_value' => '#808080',
   ),
-  6 => 
-  array (
-    'Name' => 'Brown',
-    'hex_value' => '#a52a2a',
-  ),
-  7 => 
+  3 => 
   array (
     'Name' => 'Black',
     'hex_value' => '#000000',
   ),
-  8 => 
+  4 => 
   array (
     'Name' => 'Teal',
     'hex_value' => '#008080',
+  ),
+  5 => 
+  array (
+    'Name' => 'Ass',
+    'hex_value' => '#55555',
   ),
 );
 ?>
