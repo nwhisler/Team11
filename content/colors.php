a6bbf3fce8b1ee7725d542db242150e5d21f6645
EthanSmithCS <ethansmithcs02@gmail.com>
2024-05-04 19:23:45 -0600

    coloring fix

--- content/colors.php
+++ content/colors.php
@@ -3,52 +3,47 @@ $colorsArray = array (
   0 => 
   array (
     'Name' => 'Red',
-    'hex_value' => 'ff0000',
+    'hex_value' => '#ff0000',
   ),
   1 => 
   array (
     'Name' => 'Orange',
-    'hex_value' => 'ffa500',
+    'hex_value' => '#ffa500',
   ),
   2 => 
   array (
-    'Name' => 'Yellow',
-    'hex_value' => 'ffff00',
+    'Name' => 'Green',
+    'hex_value' => '#00ff00',
   ),
   3 => 
   array (
-    'Name' => 'Green',
-    'hex_value' => '00ff00',
+    'Name' => 'Blue',
+    'hex_value' => '#0000ff',
   ),
   4 => 
   array (
-    'Name' => 'Blue',
-    'hex_value' => '0000ff',
+    'Name' => 'Purple',
+    'hex_value' => '#800080',
   ),
   5 => 
   array (
-    'Name' => 'Purple',
-    'hex_value' => '800080',
+    'Name' => 'Grey',
+    'hex_value' => '#808080',
   ),
   6 => 
   array (
-    'Name' => 'Grey',
-    'hex_value' => '808080',
+    'Name' => 'Brown',
+    'hex_value' => '#a52a2a',
   ),
   7 => 
   array (
-    'Name' => 'Brown',
-    'hex_value' => 'a52a2a',
+    'Name' => 'Black',
+    'hex_value' => '#000000',
   ),
   8 => 
   array (
-    'Name' => 'Black',
-    'hex_value' => '000000',
-  ),
-  9 => 
-  array (
     'Name' => 'Teal',
-    'hex_value' => '008080',
+    'hex_value' => '#008080',
   ),
 );
 ?>
