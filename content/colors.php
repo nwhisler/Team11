ba37d6f591b89e65a6c0c3350a737b6443d27077
Stephen Klugherz <klugherz@colostate.edu>
2024-05-05 14:21:31 -0600

    asfasfa

--- content/colors.php
+++ content/colors.php
@@ -2,33 +2,48 @@
 $colorsArray = array (
   0 => 
   array (
-    'Name' => '',
-    'hex_value' => 'asa',
+    'Name' => 'Red',
+    'hex_value' => '#ff0000',
   ),
   1 => 
+  array (
+    'Name' => 'Yellow',
+    'hex_value' => '#ffff00',
+  ),
+  2 => 
+  array (
+    'Name' => 'Green',
+    'hex_value' => '#00ff00',
+  ),
+  3 => 
+  array (
+    'Name' => 'Blue',
+    'hex_value' => '#0000ff',
+  ),
+  4 => 
   array (
     'Name' => 'Purple',
     'hex_value' => '#800080',
   ),
-  2 => 
+  5 => 
   array (
     'Name' => 'Grey',
     'hex_value' => '#808080',
   ),
-  3 => 
+  6 => 
+  array (
+    'Name' => 'Brown',
+    'hex_value' => '#a52a2a',
+  ),
+  7 => 
   array (
     'Name' => 'Black',
     'hex_value' => '#000000',
   ),
-  4 => 
+  8 => 
   array (
     'Name' => 'Teal',
     'hex_value' => '#008080',
-  ),
-  5 => 
-  array (
-    'Name' => 'Ass',
-    'hex_value' => '#55555',
   ),
 );
 ?>
